43126066e80235e686d80d1bc93d44c2ca140ef1
Valery Nevzorov <masterpiece.nevzorov@gmail.com>
2021-05-06 06:51:10 +0300

    Added UI for two screens with two variations of the UITextField.

--- Authentication/ViewController.swift
+++ Authentication/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var bottomTextFiled: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
