229ed64cd3e3d716d11a5d94d9619e54b64e4b03
Valery Nevzorov <masterpiece.nevzorov@gmail.com>
2021-05-13 14:53:41 +0300

    Fixed constraints of the UITextField and added corner radius for buttons.

--- Authentication/ViewController.swift
+++ Authentication/ViewController.swift
@@ -8,14 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    @IBOutlet weak var bottomTextFiled: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
 
 
